83e836363d9d845427a9cdd2f092531ef286726f
martinjakubec <martin.kubo8@gmail.com>
2021-06-07 18:58:28 +0200

    refactor water spawning

--- src/utils/2DArr.ts
+++ src/utils/2DArr.ts
@@ -1,9 +1,11 @@
-const make2DArr = (rows, columns): any[][] => {
+import Cell from "../terrain/Cell";
+
+const make2DArr = (rows, columns, cellContent: any = ''): any[][] => {
   let arr = [];
   for (let row = 0; row < rows; row++) {
     arr.push([]);
     for (let column = 0; column < columns; column++) {
-      arr[row][column] = ''
+      arr[row][column] = cellContent;
     }
   }
   return arr;
